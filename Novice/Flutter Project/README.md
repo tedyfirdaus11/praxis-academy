1c4b5b2f7a2d5f42324bc0f3aa58565502e96e79
Tedyfirdaus <tedyfirdaus1199@gmail.com>
2020-03-09 22:03:20 +0700

    First Flutter Project 02-01

---
+++ Novice/Flutter Project/README.md
@@ -0,0 +1,24 @@
+# Pertemuan 02-01
+
+Project flutter baru 
+
+semua tentang installer flutter dari penyiapan SDK sampai penempatan environment dari step 1 - 4 hari ini sudah dijalankan
+
+Peralatan tempur yang diperlukan : 
+
+-   Perintah Flutter Doctor : Android SDK serta config plugin di Android Studio, License, dan Flutter SDK
+-   Install Flutter dan dart di VS.Code
+-   Lakukan debugging pada hp masing2
+-   Lihatlah tampilan awal dari flutter
+
+Latihan : 
+
+Diawal kita disuguhi tampilan ksong dari halaman awal Flutter seperti gambar widget untuk menambah detik 
+
+Setelah itu kita diminta untuk membuat tampilan awal Hello World di flutter seperti gambar dibawah ini 
+
+![ini gambar 1](https://flutter.dev/assets/get-started/android/hello-world-fadb9765c01d33f0fea92d7ac767e036ea90e9159335ea1841277bc6bef3a10a.png)
+
+lalu jika sudah maka kita bisa lanjut untuk menyelesaikan step berikutnya dengan mengikuti tutorial yang sudah ada di Web Flutter sampai selesai.
+
+Untuk fungsi fungsi nya besok akan kita pelajari lebih lanjut tentang stuktural flutter.
---
+++ Novice/Flutter Project/tedy/lib/main.dart
@@ -0,0 +1,127 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp()); // metode satu baris
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return MaterialApp(
+      //title: 'Welcome to Flutter',
+      // home: Scaffold(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: RandomWords(),
+      //  // appBar: AppBar(
+      //     //menyediakan judul pada aplikasi bar diatas
+      //    // title: Text('Welcome to Ruang tidak Ekspresi'),
+      // //  ),
+      // body: Center(
+      // //    child: Text(wordPair.asPascalCase),
+      //     child: RandomWords(),
+      //   ),
+      // ),
+    );
+  }
+} // stateless
+
+class RandomWords extends StatefulWidget {
+  //tekan stf
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
+  // final _suggestions = <WordPair>[];
+  // final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
